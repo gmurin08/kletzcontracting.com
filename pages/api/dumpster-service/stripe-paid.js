6721da0568fd14f1ff3251ab4cfab3db9359589f
Gino Murin <gmurin@gmail.com>
2025-05-18 23:39:27 -0400

    updated footer and home faq

--- pages/api/dumpster-service/stripe-paid.js
+++ pages/api/dumpster-service/stripe-paid.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import nodemailer from 'nodemailer';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 export const config = {
   api: {
