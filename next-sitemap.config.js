c013c7e5d71927b453092933b3aac8bb069b9f82
Gino Murin <gmurin@gmail.com>
2025-05-21 06:37:09 -0400

    removed more template routes from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,13 +2,19 @@
 module.exports = {
   siteUrl: 'https://kletzcontracting.com',
   generateRobotsTxt: true,
-  exclude: ['/api/*', '/admin/*', '/dashboard/*', '/blog', '/blog/*'],
+  exclude: ['/api/*', '/admin/*', '/dashboard/*', '/blog', '/blog/*', '/OLDservices', 
+    'blog-details', 'opportunity-updated', 'project', 'project-details', 'services-details',
+    'team', 'team-details'
+  ],
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/admin/', '/dashboard/', '/blog/', '/blog/*'],
+        disallow: ['/api/', '/admin/', '/dashboard/', '/blog/', '/blog/*', '/OLDservices', 
+    'blog-details', 'opportunity-updated', 'project', 'project-details', 'services-details',
+    'team', 'team-details'
+  ],
       },
     ],
   },
