38597d1684abaf1355e9660fbf751c79fbfc50e2
Gino Murin <gmurin@gmail.com>
2026-02-16 07:59:47 -0500

    added victor to email recipeints

--- pages/api/submit-contact-form.js
+++ pages/api/submit-contact-form.js
@@ -340,7 +340,7 @@ async function sendContactNotification(contactDetails) {
   // Send email
   const info = await transporter.sendMail({
     from: process.env.SMTP_FROM,
-    to: process.env.CLIENT_EMAIL, // Send to yourself as notification
+    to: `${process.env.CLIENT_EMAIL}, victor@kletzcontracting.com`,
     subject: `New Contact Form Submission - ${contactDetails.firstName} ${contactDetails.lastName}`,
     html: emailHTML,
   });
